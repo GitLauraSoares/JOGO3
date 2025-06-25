43aa5031861ff367ec7b31902969f2867dd7ec7d
Laura Soares <137304558+GitLauraSoares@users.noreply.github.com>
2025-06-25 14:26:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -145,14 +145,14 @@
   function salvarPlacar(nome, tempo, erros) {
   const payload = { nome, tempo, erros };
 
-  fetch("https://script.google.com/macros/s/AKfycbzEHFIpfR5FoJ4zx06BqmxxePwu8NEml5FOTJyPcWX27U0xz3SY8in1gRvNUT-NuOxS/exec", {
-    method: "POST",
-    body: JSON.stringify(payload),
-    headers: { "Content-Type": "application/json" }
-  })
-  .then(res => res.text())
-  .then(texto => console.log("Resposta do servidor:", texto))
-  .catch(erro => console.error("Erro ao enviar placar:", erro));
+  fetch("https://trilha.laurasoares-soaress.workers.dev/", { // <-- URL do Worker, não do Apps Script
+  method: "POST",
+  body: JSON.stringify(payload),
+  headers: { "Content-Type": "application/json" }
+})
+.then(res => res.text())
+.then(texto => console.log("Resposta do servidor:", texto))
+.catch(erro => console.error("Erro ao enviar placar:", erro));
 }
 
 
